ab0b503fbdc843bcc9595d2dbd55e4bac070fdf9
Henry <henry.solito@catolica.edu.sv>
2023-05-21 14:15:08 -0600

    Add project files.

---
+++ ProyectoDAW_#8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoDAW__8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
