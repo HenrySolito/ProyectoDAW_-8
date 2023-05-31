1d5806319346ab56382a8d58025e59c8fc2b91e8
Henry <henry.solito@catolica.edu.sv>
2023-05-31 16:43:59 -0600

    controller y vista de Ofertas

---
+++ ProyectoDAW_#8/Controllers/OfertasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoDAW__8.Controllers
+{
+    public class ofertasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
