0a8048c6211f51c0e0ddbc32d6312e24909a9571
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-06-03 08:07:44 -0600

    Agregado de vista

--- ProyectoDAW_#8/Controllers/OfertasController.cs
+++ ProyectoDAW_#8/Controllers/OfertasController.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoDAW__8.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ProyectoDAW__8.Controllers
 {
     public class ofertasController : Controller
     {
         private readonly empleosContext _empleosContext;
+        private readonly ILogger<ofertasController> _logger;
 
-        public ofertasController(empleosContext empleosContext)
+        public ofertasController(ILogger<ofertasController> logger, empleosContext empleosContext)
         {
+            _logger = logger;
             _empleosContext = empleosContext;
         }
-        public IActionResult Index()
+
+            public IActionResult Index()
         {
             var OfertaGeneral = (from m in _empleosContext.ofertas
                                 select new {
